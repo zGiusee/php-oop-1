13f92e0c24438fed006e8fa6622125498dcb8233
zGiusee <giusebelforte@gmail.com>
2024-01-30 14:57:02 +0100

    [UPDATE] Base exercise done

--- index.php
+++ index.php
@@ -3,20 +3,48 @@
 // Definisco la classe Movie
 class Movies
 {
+    // Parametri della classe
     public $title;
-    public $year;
     public $genre;
     public $duration;
+    public $year;
 
-    function __construct($_title, $_year, $_genre, $_duration)
+    // Funzione construct
+    function __construct($_title, $_genre, $_duration)
     {
         $this->title = $_title;
-        $this->year = $_year;
         $this->genre = $_genre;
         $this->duration = $_duration;
     }
+
+    // Funzione per esercizio 
+    public function setFilmYear($year)
+    {
+        if ($year > 2020) {
+            $this->year = 'Film recente';
+        } else {
+            $this->year = 'Film non recente';
+        }
+    }
 };
 
+// Aggiungo le istanze alla classe Movie
+$movie_1 = new Movies('L ultima volta che siamo stati bambini', 'Commedia', '1h 30min');
+$movie_2 = new Movies('Borg McEnroe', 'Sportivo', '1h 48min');
+$movie_3 = new Movies('Senza Freni', 'Azione', '1h 31min');
+
+// Inserisco la data applicando la funzione
+$movie_1->setFilmYear(2023);
+$movie_1->setFilmYear(2017);
+$movie_1->setFilmYear(2013);
+
+
+// Inserisco le istanze in un array per ciclarle
+$moviesArray = [
+    $movie_1,
+    $movie_2,
+    $movie_3
+];
 
 ?>
 <!DOCTYPE html>
@@ -32,8 +60,16 @@ class Movies
 <body>
     <main>
         <div class="container">
-            <div class="row">
-
+            <div class="row py-5">
+                <?php foreach ($moviesArray as $movie) { ?>
+                    <div class="col-4">
+                        <div class="text-center">
+                            <h1><?php echo $movie->title ?></h1>
+                            <h3><?php echo "Genere:" . $movie->genre ?></h3>
+                            <h5><?php echo  $movie->year . " Durata del film:" . $movie->duration ?></h5>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
         </div>
     </main>
