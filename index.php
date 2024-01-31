cd89466adb8b173b5c2b1f765452c7644380b4a5
zGiusee <giusebelforte@gmail.com>
2024-01-31 09:44:32 +0100

    [UPDATE] aggiunta un altra classe per incapsulamento classi

--- index.php
+++ index.php
@@ -7,14 +7,17 @@ class Movies
     public $title;
     public $genre;
     public $duration;
+    public $casa;
     public $year;
+    public $voto;
 
     // Funzione construct
-    function __construct($_title, $_genre, $_duration)
+    function __construct($_title, $_genre, $_duration, Voto $_voto)
     {
         $this->title = $_title;
         $this->genre = $_genre;
         $this->duration = $_duration;
+        $this->voto = $_voto;
     }
 
     // Funzione per esercizio 
@@ -28,10 +31,29 @@ class Movies
     }
 };
 
+// Definisco la classe Voto di Movie
+class Voto
+{
+    public $voto;
+    public $casa;
+
+    function __construct($_voto, $_casa)
+    {
+        $this->voto = $_voto;
+        $this->casa = $_casa;
+    }
+};
+
+$voto_movie_1 = new Voto(5, 'Movie DB');
+$voto_movie_2 = new Voto(3.3, 'Movie DB');
+$voto_movie_3 = new Voto(2, 'Movie DB');
+
+
+
 // Aggiungo le istanze alla classe Movie
-$movie_1 = new Movies('L ultima volta che siamo stati bambini', ['Commedia'], '1h 30min');
-$movie_2 = new Movies('Borg McEnroe', ['Sportivo', 'Thriller'], '1h 48min');
-$movie_3 = new Movies('Senza Freni', ['Azione', 'Horror'], '1h 31min');
+$movie_1 = new Movies('L ultima volta che siamo stati bambini', ['Commedia'], '1h 30min', $voto_movie_1);
+$movie_2 = new Movies('Borg McEnroe', ['Sportivo', 'Thriller'], '1h 48min', $voto_movie_2);
+$movie_3 = new Movies('Senza Freni', ['Azione', 'Horror'], '1h 31min', $voto_movie_3);
 
 // Inserisco la data applicando la funzione
 $movie_1->setFilmYear(2023);
@@ -72,6 +94,9 @@ $moviesArray = [
                             </h3>
 
                             <h5><?php echo  $movie->year . " Durata del film:" . $movie->duration ?></h5>
+
+                            <h5 class="pt-2  "><?php echo "Casa:" . $movie->voto->casa ?></h5>
+                            <h5><?php echo "Voto:" . $movie->voto->voto ?></h5>
                         </div>
                     </div>
                 <?php } ?>
